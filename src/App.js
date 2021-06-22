5f945a55d8d88c0ffda625639158a584171c569d
Gunasekaran-03 <72186148+Gunasekaran-03@users.noreply.github.com>
2021-06-22 11:15:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Home from "./homepage";
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+}from "react-router-dom";
+import Register from "./register";
+import Reset from "./resetpassword";
+import Update from "./updatepassword";
+import Profile from "./profile";
+
+
+function App() {
+  return (
+    <>
+    <Router>
+      <div className="container-fluid">
+        <div className="row no-gutter">
+          <div className="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+          <div className="col-md-8 col-lg-6">
+            <div className="login align-items-center py-5">
+              <div className="container">
+                <Switch>
+                  <Route path="/" component={Home} exact={true} />
+                  <Route path="/register" component={Register} exact={true}/>
+                  <Route path="/reset-pass" component={Reset} exact={true}/>
+                  <Redirect to="/update-password" component={Update} exact={true}/>
+                  <Route path="/profile" component={Profile} exact={true}/>
+                </Switch>
+              
+              </div>
+            </div>
+           
+          </div>
+        </div>       
+      </div>
+      {/* <hr/>
+      <div classNameName="container-fluid mt-3">
+        <div classNameName="row">
+        <Switch>
+                
+        </Switch>
+        </div>
+      </div> */}
+      </Router>
+    </>
+  );
+}
+
+export default App;
